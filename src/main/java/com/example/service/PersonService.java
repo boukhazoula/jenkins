7a0edb55d8ac97f26d1ac59fdb35bc1d7078c568
Haitem_Boukhazoula <107353498+boukhazoula@users.noreply.github.com>
2024-01-04 16:38:51 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -14,7 +14,7 @@ public class PersonService {
 	public PersonService() {
 		Person p = new Person();
 		p.setId("1");
-		p.setAge(24(21);
+		p.setAge(2421);
 		p.setFirstName("KhGadlCed");
 		p.setLastName("Amrosy");
 		persons.add(p);
