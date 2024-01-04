09dbfb3ffe6b9b0f678765c1d02ca89c0217eff6
Haitem_Boukhazoula <107353498+boukhazoula@users.noreply.github.com>
2024-01-04 11:24:43 +0100

    add new person

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(28);
+		p.setFirstName("Haitem");
+		p.setLastName("Boukhazoula");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
